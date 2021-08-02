f31e8b1868b1308e82a6f72168fd2ac3286b61a7
Raffael Schneider <raffael.schneider@b-nova.com>
2021-08-02 10:53:13 +0200

    - added port config per env var - added Procfile for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar ./build/libs/slackbot-1.0.0.jar
--- src/main/kotlin/com/bnova/Application.kt
+++ src/main/kotlin/com/bnova/Application.kt
@@ -6,7 +6,8 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    val port = System.getenv("PORT").toIntOrNull() ?: 8080
+    embeddedServer(Netty, port, host = "0.0.0.0") {
         module()
         configureSerialization()
     }.start(wait = true)
